930c682a086d543c8a360306d8ea4d5172304888
Guilherme Augusto Raduenz <graduenz@gmail.com>
2022-11-23 15:00:21 -0300

    chore: changed data provider to use local API

--- src/admin/index.tsx
+++ src/admin/index.tsx
@@ -1,12 +1,12 @@
 import { Admin, Resource, ListGuesser } from 'react-admin';
-import jsonServerProvider from 'ra-data-json-server';
+import dataProviderFactory from './dataProvider';
 
-const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
+const dataProvider = dataProviderFactory('http://localhost:5036');
 
 const App = () => (
   <Admin dataProvider={dataProvider}>
-    <Resource name="posts" list={ListGuesser} />
-    <Resource name="comments" list={ListGuesser} />
+    <Resource name="v1/organizations" options={{ label: 'Organizations' }} list={ListGuesser}  />
+    <Resource name="v1/donators" options={{ label: 'Donators' }} list={ListGuesser} />
   </Admin>
 );
 
